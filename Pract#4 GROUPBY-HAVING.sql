33aa3d2b6fc6f48bb15c4473b6499eea8be2a2a7
cgallo <tKgue743>
2025-09-25 11:01:56 -0300

    agrego práctica de funciones de agregación y agrupamiento

---
+++ Pract#4 GROUPBY-HAVING.sql
@@ -0,0 +1,51 @@
+-- Practica Nº 4: GROUP BY - HAVING
+-- Practica en Clase: 1 – 2 – 4 – 8 – 9 – 13 – 14 – 15
+-- Práctica Complementaria: 3 – 5 – 6 – 7 – 10 – 11 – 12
+-- BASE DE DATOS: AGENCIA_PERSONAL
+
+-- **1) Mostrar las comisiones pagadas por la empresa Tráigame eso
+
+-- **2) Ídem 1) pero para todas las empresas.
+
+-- 3) Mostrar el promedio, desviación estándar y varianza del puntaje de las
+-- evaluaciones de entrevistas, por tipo de evaluación y entrevistador. Ordenar por promedio
+-- en forma ascendente y luego por desviación estándar en forma descendente.
+
+-- **4) Ídem 3) pero para Angélica Doria, con promedio mayor a 71. Ordenar por código
+-- de evaluación.
+
+-- 5) Cuantas entrevistas fueron hechas por cada entrevistador en octubre de 2014.
+
+-- 6) Ídem 4) pero para todos los entrevistadores. Mostrar nombre y cantidad.
+-- Ordenado por cantidad de entrevistas.
+
+-- 7) Ídem 6) para aquellos cuya cantidad de entrevistas por código de evaluación
+-- sea mayor que 1. Ordenado por nombre en forma descendente y por código de
+-- evaluación en forma ascendente
+
+-- **8) Mostrar para cada contrato cantidad total de las comisiones, cantidad a pagar,
+-- cantidad a pagadas.
+
+-- **9) Mostrar para cada contrato la cantidad de comisiones, el % de comisiones pagas y
+-- el % de impagas.
+
+-- 10) Mostar la cantidad de empresas diferentes que han realizado solicitudes y la
+-- diferencia respecto al total de solicitudes.
+
+
+-- 11) Cantidad de solicitudes por empresas.
+
+-- 12) Cantidad de solicitudes por empresas y cargos.
+
+-- LEFT/RIGHT JOIN
+-- **13) Listar las empresas, indicando todos sus datos y la cantidad de personas diferentes
+-- que han mencionado dicha empresa como antecedente laboral. Si alguna empresa NO fue
+-- mencionada como antecedente laboral deberá indicar 0 en la cantidad de personas.
+
+
+-- **14) Indicar para cada cargo la cantidad de veces que fue solicitado. Ordenado en
+-- forma descendente por cantidad de solicitudes. Si un cargo nunca fue solicitado, mostrar
+-- 0. Agregar algún cargo que nunca haya sido solicitado
+
+
+-- **15) Indicar los cargos que hayan sido solicitados menos de 2 veces
